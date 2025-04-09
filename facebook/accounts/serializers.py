33ed5ff690f6de97aad795b901fea01b102dae96
Barry-owino <barryowino5@gmail.com>
2025-04-09 20:02:09 +0300

    fixed the registration error on serializers,urls and settings.py

--- facebook/accounts/serializers.py
+++ facebook/accounts/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
         def create(self, validated_data):
             return User.objects.create_user(**validate_data)
